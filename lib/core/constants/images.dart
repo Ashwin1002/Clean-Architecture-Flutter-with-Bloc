21f346d85232ecfd19da45434719b6b06e28caea
Ashwin1002 <ashwin.shrestha2258@gmail.com>
2024-01-10 16:16:19 +0545

    logo configuration

---
+++ lib/core/constants/images.dart
@@ -0,0 +1,26 @@
+class AssetList {
+  static const String iconPath = 'assets/icons/';
+  static const String imagePath = 'assets/images/';
+  static const String lottiePath = 'assets/lottie/';
+
+  ///icons
+  static const String addIcon = '$iconPath/add.svg';
+  static const String appleIcon = '$iconPath/appleIcon.svg';
+  static const String arrowDownIcon = '$iconPath/arrow_down.svg';
+  static const String arrowRightIcon = '$iconPath/arrow_right.svg';
+  static const String closeIcon = '$iconPath/close.svg';
+  static const String dashboardIcon = '$iconPath/dashboard.svg';
+  static const String dateIcon = '$iconPath/date.svg';
+  static const String googleIcon = '$iconPath/google.svg';
+  static const String hidePassIcon = '$iconPath/hidePassIcon.svg';
+  static const String homeIcon = '$iconPath/home.svg';
+  static const String menuIcon = '$iconPath/menu.svg';
+  static const String moreIcon = '$iconPath/more.svg';
+  static const String phoneIcon = '$iconPath/phone.svg';
+  static const String refreshIcon = '$iconPath/refresh.svg';
+  static const String searchIcon = '$iconPath/search.svg';
+  static const String showPassIcon = '$iconPath/show_pass.svg';
+
+  ///app logo
+  static const String appLogoNoBG = '$imagePath/app_icon.svg';
+}
